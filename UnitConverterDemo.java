d8f02ea30f56ef11558574922e7cfef51f8ff7cb
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-04 11:51:34 +0530

    [sujoy] desc: Demo program to demonstrate the UnitConverter utility class

---
+++ UnitConverterDemo.java
@@ -0,0 +1,10 @@
+public class UnitConverterDemo {
+
+    public static void main(String[] args) {
+        // Example conversions
+        System.out.println("10 km to miles: " + UnitConverter.convertKmToMiles(10));
+        System.out.println("5 miles to km: " + UnitConverter.convertMilesToKm(5));
+        System.out.println("3 meters to feet: " + UnitConverter.convertMetersToFeet(3));
+        System.out.println("12 inches to cm: " + UnitConverter.convertInchesToCm(12));
+    }
+}
